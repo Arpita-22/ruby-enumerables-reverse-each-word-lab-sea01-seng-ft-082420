b6f11d71e57f6bebd9124661ab644dab48184be9
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-24 01:07:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
   string_array = string.split("")
-  reverse_array = []
-  string_array.collect do |char|
-    reverse_array = string_array.pop()
-  end
+  string_array.collect do |joined_array|
+    joined_array.reverse
+  end.join(' ')
 end
