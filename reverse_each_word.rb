cb46a42eb23ad3954b612fdd9819dda5523b09b9
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-24 01:08:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
-  string_array = string.split("")
-  string_array.collect do |joined_array|
+  string_array.split.collect do |joined_array|
     joined_array.reverse
   end.join(' ')
 end
